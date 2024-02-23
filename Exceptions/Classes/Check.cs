585bdc8f1e1a21a846720b65e94283e7b0a9efff
JaloliddinDeveloper <jaloliddin.mahkamov@mail.ru>
2024-02-23 20:39:47 +0500

    Add project files.

---
+++ Exceptions/Classes/Check.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions.Classes
+{
+    internal class Check
+    {
+        public void CheckMethod(string age) 
+        {
+            age = Console.ReadLine();
+            while (!TypeCheckInt(age))
+            {
+                Console.WriteLine("Yoshingizni sonda kiriting:");
+                age = Console.ReadLine();
+            }
+             bool TypeCheckInt(String input)
+            {
+                int number = 0;
+                return int.TryParse(input, out number);
+            }
+        }
+    }
+        
+}
---
+++ Exceptions/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ismingizni kiriting: ");
+        string name = Console.ReadLine();
+       Console.WriteLine("Yoshingizni kiriting:");
+        string age =Console.ReadLine();
+        while(!TypeCheckInt(age))
+        {
+            Console.WriteLine("Yoshingizni sonda kiriting:");
+            age = Console.ReadLine();
+        }
+        Console.WriteLine($"Ismingiz {name}. Yoshingiz {age}.");
+    }
+    static bool TypeCheckInt(String input)
+    {
+        int number = 0;
+        return int.TryParse(input, out number);
+    }
+}
